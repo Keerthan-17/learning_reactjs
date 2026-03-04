7ab33d310d70f7a3e07e30151ff5a33515bf7b73
Keerthan M <kituk2004@gmail.com>
2026-03-05 00:09:04 +0530

    updated readme file for github.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Card from './components/card'
 const App = () => {
   return (
     <div className='parent'>
-      <Card user='Mike' des='Lorem ipsum dolor sit amet consectetur'/><Card user='Keerthu' des='Lorem ipsum dolor sit amet consectetur'/>
+      <Card user='Mike' des='Lorem ipsum dolor sit amet consectetur'/>
+      <Card user='Keerthu' des='Lorem ipsum dolor sit amet consectetur'/>
       <Card user='Manu' des='Lorem ipsum dolor sit amet consectetur'/>
     </div>
   )
