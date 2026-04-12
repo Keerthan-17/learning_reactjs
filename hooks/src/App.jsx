d76c679baa7198c2459ae78ef0bebd0e03770e3a
Keerthan M <kituk2004@gmail.com>
2026-04-12 21:04:32 +0530

    learn batch managemnet in useState.

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,6 +1,27 @@
+import { useState } from "react"
 const App = () => {
+
+  const [data, setdata] = useState({user:'Mike', age:20})
+
+  // const updatevalue = ()=>{
+  //   const newData = {...data}
+  //   newData.user = 'Keerthu'
+  //   newData.age = '21'
+
+  //   setdata(newData)
+  // }
+
+  // or we could use arrow function to directly change value
+
+  const updatevalue = ()=>{
+    setdata(prev=>({...prev,age:22}))
+  }
+
   return (
-    <div> </div>
+    <div>
+      <h1>Name: {data.user}, Age:{data.age}</h1>
+      <button onClick={updatevalue}>Click Me!!</button>
+    </div>
   )
 }
 
